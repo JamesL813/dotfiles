cbbe356877d06a65515c4f645d142e47570284bd
James Lawton <JCL813@proton.me>
2022-11-03 19:06:34 -0400

    keybindings

--- NvChad/lua/custom/mappings.lua
+++ NvChad/lua/custom/mappings.lua
@@ -4,7 +4,7 @@ local M = {}
 -- add this table only when you want to disable default keys
 M.disabled = {
 	n = {
-		["<leader>h"] = "",
+		-- ["<leader>h"] = "",
 	},
 }
 
@@ -18,8 +18,25 @@ M.general = {
 		["J"] = { "}", "jump down a paragraph" },
 		["K"] = { "{", "jump up a paragraph" },
 		["L"] = { "$", "jump to the end of a line" },
-		["<Tab>"] = { ">>", "Indent line" },
+		["<TAB>"] = { ">>", "Indent line" },
 		["<S-Tab>"] = { "<<", "Unindent line" },
+
+		["."] = {
+			function()
+				require("nvchad_ui.tabufline").tabuflineNext()
+			end,
+			"goto next buffer",
+		},
+		[","] = {
+			function()
+				require("nvchad_ui.tabufline").tabuflinePrev()
+			end,
+			"goto previous buffer",
+		},
+
+		[";"] = { ":", "enter cmdline", opts = { nowait = true } },
+
+		["<leader>v"] = { "<cmd> vs <CR>", "vertical split" },
 	},
 
 	v = {
