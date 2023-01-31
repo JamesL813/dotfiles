eef76a2d76c5dd86aafb34acd02d58d5d878d0e8
James Lawton <JamesL81399@gmail.com>
2023-01-31 13:30:23 -0500

    minor changes

--- install.sh
+++ install.sh
@@ -2,11 +2,11 @@
 
 # basic
 sudo pacman -Syu
+sudo pacman -S --needed base-devel
 sudo pacman -S git neovim firefox htop zsh openssh
 chsh -s $(which zsh)
 
 # aur helper paru
-sudo pacman -S --needed base-devel
 git clone https://aur.archlinux.org/paru.git
 cd paru
 makepkg -si
@@ -15,10 +15,10 @@ makepkg -si
 paru -S zsh-autosuggestions zsh-completions zsh-history-substring-search zsh-syntax-highlighting zsh-theme-powerlevel10k
 
 # rust replacements
-paru -S rustup exa bat rg fd sd bottom dust procs diskonaut tealdeer
+paru -S rustup exa bat ripgrep ripgrep-all fd sd bottom dust procs diskonaut tealdeer
 
 # cli tools
-paru -S lf fzf translate-shell
+paru -S lf fzf translate-shell dictd dict-gcide youtube-dl
 
 # other
 paru -S enpass-bin protonvpn-cli qbittorrent
